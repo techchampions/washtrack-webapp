60d2fbccf93e40de14cd9caba965b790dc452cd7
King Soorsa <kingsoorsa@gmail.com>
2025-11-20 14:45:40 +0100

    Soosar2 (#14)
    * start to setup folder structure and file and code
    
    * still updating project setup
    
    * setup project for signup flow
    
    * auth flow: the otp screen done
    
    * auth flow: add services in progress
    
    * add error handling for store update
    
    * updating add and edit item types
    
    * onboarding and auth flow restructure not ui
    
    * update login & logout logic
    
    * implement the state for auth flow
    
    * update dashbaord screen
    
    * revamp in progress
    
    * revamp multiple ts config files and add paths
    
    * rebuilding ongoing and basic login working
    
    * update loginform
    
    * implement sign up and login formate
    
    * verify email otp page implmentation
    
    * update verify email code
    
    * add screens for onboarding
    
    * auth flow feature
    
    * starting the onboarding flow
    
    * store profile setup ui done
    
    * fix button size store profile setup file
    
    * add location implementation
    
    * store profile setup completion done
    
    * add services setup done
    
    * ui, logic, crud for add items setup
    
    * complete  add item services
    
    * congratulation  screen in onboarding completed
    
    * start connecting the screens
    
    * link the pages
    
    * update codebase
    
    * initail commit
    
    * onboding
    
    * fdfgu
    
    * djkdfj
    
    * djkdfj
    
    * fsfsf
    
    * UIUQEBI
    
    * GFHFHFH
    
    * SFGDGDG
    
    * DGDGWTEYRG
    
    * DSGSHSHSSFH
    
    * SFDGETFGG
    
    * FDGDGTRWC
    
    * SFFEETETER
    
    * SFFEETETERssd
    
    * EREFGFHFHFG
    
    * FWRWTWTE
    
    * RRBFGFG
    
    * added complete order process and view item images
    
    * added new button on success modal
    
    * added loading modal for status
    
    * change btn color
    
    * redirect order overview
    
    * added forgot password & netlify config
    
    * fixed change password
    
    * fixed static files dir
    
    * fix accout setting and mobile display
    
    * fixed minor bugs and responsivenesss
    
    * change theme and mobile res
    
    * media files fix
    
    * changes in app.tsx
    
    * second change to app.tsx
    
    * 4th change to app.tsx
    
    * contact page
    
    * fix add order screen
    
    ---------
    
    Co-authored-by: paulayo93 <52790286+paulayo93@users.noreply.github.com>

--- src/pages/settings/Contact.tsx
+++ src/pages/settings/Contact.tsx
@@ -15,13 +15,13 @@ const Contact = () => {
     switch (item.name) {
       case "email":
         return `mailto:${item.value}`;
-      case "phone": // Remove any non-digit characters for tel link
-      {
+      case "phone": {
+        // Remove any non-digit characters for tel link
         const phoneNumber = item.value.replace(/\D/g, "");
         return `tel:${phoneNumber}`;
       }
-      case "website": // Ensure website has proper protocol
-      {
+      case "website": {
+        // Ensure website has proper protocol
         const website = item.value.startsWith("http")
           ? item.value
           : `https://${item.value}`;
@@ -39,11 +39,14 @@ const Contact = () => {
       <Header />
       <div className="flex flex-col gap-6 lg:flex-row">
         <div className="w-full md:w-2/3">
-          <h2>Contact US</h2>
+          <h2 className="text-2xl font-brand-bold text-left mb-5">
+            Contact Us
+          </h2>
           <div className="space-y-2">
             {contacts.map((item, i) => (
               <Link
                 to={getContactLink(item)}
+                target="_blank"
                 key={i}
                 className="flex items-center gap-2 p-4 text-left cursor-pointer bg-brand-200 rounded-xl text-quick-action-icon"
               >
