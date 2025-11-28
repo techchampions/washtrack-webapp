33c1cbd31345fefda570a40110412e0033072338
John Dev <kingsoorsa@gmail.com>
2025-11-28 16:38:28 +0100

    added creation date and auto update cost of services

--- src/pages/orders/AddOrderExistingUser.tsx
+++ src/pages/orders/AddOrderExistingUser.tsx
@@ -37,7 +37,13 @@ export const AddOrderExistingUser: React.FC = () => {
   const { data, isLoading } = useGetCustomerProfile(user_id || "");
   const { data: orderItemData } = useGetOrderItem();
   const orderItems = orderItemData?.Items ?? [];
+  const cost_of_service = orderItems.reduce(
+    (x, item) => x + Number(item.price),
+    0
+  );
+
   const initialValues = {
+    creationDate: new Date(),
     pickupDate: new Date(),
     payment_type: "cash",
     costOfService: 0,
@@ -142,133 +148,152 @@ export const AddOrderExistingUser: React.FC = () => {
               onSubmit={handleSubmit}
               // enableReinitialize
             >
-              {({ isValid, values }) => (
-                <Form className="w-full space-y-2">
-                  {isLoading ? (
-                    <OrderItemLoading count={1} />
-                  ) : (
-                    <div className="flex items-center justify-between px-4 py-2 rounded-lg bg-brand-100">
-                      <div className="flex items-center gap-4">
-                        <img
-                          src="/images/user-icon.png"
-                          alt="User"
-                          className="w-10 h-10 rounded-full"
-                        />
-                        <div className="text-left">
-                          <p className="font-bold text-quick-action-icon">
-                            {data?.customer.name}
-                          </p>
-                          <p className="text-sm text-gray-500">
-                            {data?.customer.phone_number} -{" "}
-                            {data?.customer.email}
-                          </p>
+              {({ isValid, values, setFieldValue }) => {
+                // eslint-disable-next-line react-hooks/rules-of-hooks
+                React.useEffect(() => {
+                  setFieldValue("costOfService", cost_of_service);
+                  // eslint-disable-next-line react-hooks/exhaustive-deps
+                }, [cost_of_service, setFieldValue]);
+                return (
+                  <Form className="w-full space-y-2">
+                    {isLoading ? (
+                      <OrderItemLoading count={1} />
+                    ) : (
+                      <div className="flex items-center justify-between px-4 py-2 rounded-lg bg-brand-100">
+                        <div className="flex items-center gap-4">
+                          <img
+                            src="/images/user-icon.png"
+                            alt="User"
+                            className="w-10 h-10 rounded-full"
+                          />
+                          <div className="text-left">
+                            <p className="font-bold text-quick-action-icon">
+                              {data?.customer.name}
+                            </p>
+                            <p className="text-sm text-gray-500">
+                              {data?.customer.phone_number} -{" "}
+                              {data?.customer.email}
+                            </p>
+                          </div>
                         </div>
+                        <FaChevronRight className="text-quick-action-icon" />
                       </div>
-                      <FaChevronRight className="text-quick-action-icon" />
-                    </div>
-                  )}
+                    )}
 
-                  {/* Added Item */}
-                  <div className="flex items-center justify-between w-full py-3 mt-2 text-black">
-                    <div className="font-bold text-[16px]">Add Item</div>
-                    <FiPlusCircle
-                      className="w-8 h-8 text-black cursor-pointer"
-                      onClick={() => {
-                        modal.openModal(<AddItemForOrder />);
-                      }}
-                    />
-                  </div>
-
-                  {orderItems.map((item, index) => (
-                    <div
-                      className="flex items-center justify-between px-4 py-2 mt-1 rounded-lg cursor-pointer bg-brand-100"
-                      key={index}
-                      onClick={() => handleEditItem(item)}
-                    >
-                      <div className="flex items-center gap-4">
-                        <img
-                          src="/images/order-icon.png"
-                          className="object-cover w-10 h-10 rounded"
-                          alt={item.item_type}
-                        />
-                        <div className="text-left">
-                          <p className="font-semibold text-quick-action-icon">
-                            {item.service_name}
-                          </p>
-                          <p className="text-sm text-gray-500">
-                            {item.item_type} - {item.no_of_items} pieces
-                          </p>
-                        </div>
-                      </div>
-                      <div className="">
-                        <div className="flex items-center justify-end gap-1 text-sm cursor-pointer text-quick-action-icon hover:text-blue-700">
-                          <ChevronRight />
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-
-                  {/* Pickup Date */}
-                  <div className="mt-4 text-left">
-                    <DatePickerInput label="Pickup Date" name="pickupDate" />
-                  </div>
-
-                  {/* Payment Details */}
-                  <div className="text-black font-bold text-[16px] mb-0 text-left mt-5">
-                    Payment Method
-                  </div>
-                  <div className="flex items-center gap-1 text-sm text-gray-300">
-                    <Info size={15} />
-                    <span>Please select preffered payment method</span>
-                  </div>
-                  <div className="mb-5 ml-5">
-                    <RadioSelect
-                      name="payment_type"
-                      options={PAYMENT_OPTIONS}
-                    />
-                  </div>
-                  <div className="mt-4 bg-brand-100 p-4 mb-4 rounded-lg text-[12px]">
-                    <div className="grid grid-cols-2 py-2 text-black gap-y-3 md:grid-cols-3">
-                      <div className="md:col-span-2 justify-self-start">
-                        Cost of service
-                      </div>
-                      <InputFieldFormatted
-                        formatAsNaira
-                        name="costOfService"
-                        className="justify-self-end "
+                    {/* Added Item */}
+                    <div className="flex items-center justify-between w-full py-3 mt-2 text-black">
+                      <div className="font-bold text-[16px]">Add Item</div>
+                      <FiPlusCircle
+                        className="w-8 h-8 text-black cursor-pointer"
+                        onClick={() => {
+                          modal.openModal(<AddItemForOrder />);
+                        }}
                       />
-                      <div className="md:col-span-2 justify-self-start">
-                        Amount Paid
+                    </div>
+
+                    {orderItems.map((item, index) => (
+                      <div
+                        className="flex items-center justify-between px-4 py-2 mt-1 rounded-lg cursor-pointer bg-brand-100"
+                        key={index}
+                        onClick={() => handleEditItem(item)}
+                      >
+                        <div className="flex items-center gap-4">
+                          <img
+                            src="/images/order-icon.png"
+                            className="object-cover w-10 h-10 rounded"
+                            alt={item.item_type}
+                          />
+                          <div className="text-left">
+                            <p className="font-semibold text-quick-action-icon">
+                              {item.service_name}
+                            </p>
+                            <p className="text-sm text-gray-500">
+                              {item.item_type} - {item.no_of_items} pieces
+                            </p>
+                          </div>
+                        </div>
+                        <div className="">
+                          <div className="flex items-center justify-end gap-1 text-sm cursor-pointer text-quick-action-icon hover:text-blue-700">
+                            <ChevronRight />
+                          </div>
+                        </div>
                       </div>
-                      <InputFieldFormatted
-                        formatAsNaira
-                        name="amountPaid"
-                        className="justify-self-end"
+                    ))}
+
+                    {/* Pickup Date */}
+                    <div className="mt-4 text-left">
+                      <DatePickerInput
+                        label="Creation Date"
+                        name="creationDate"
+                        maxDate={new Date()}
                       />
-                      <div className="justify-self-start md:col-span-2">
-                        Balance
-                      </div>
-                      <div className="font-bold justify-self-end">
-                        {formatPrice(
-                          (values.costOfService || 0) - (values.amountPaid || 0)
-                        )}
-                      </div>
-                      <div className="flex justify-between col-span-2 p-2 mt-2 font-bold text-black bg-white border border-gray-300 rounded-lg md:col-span-3">
-                        <span>Total Amount</span>
-                        <span>{formatPrice(values.costOfService || 0)}</span>
+                      <DatePickerInput
+                        label="Pickup Date"
+                        name="pickupDate"
+                        minDate={new Date()}
+                      />
+                    </div>
+
+                    {/* Payment Details */}
+                    <div className="text-black font-bold text-[16px] mb-0 text-left mt-5">
+                      Payment Method
+                    </div>
+                    <div className="flex items-center gap-1 text-sm text-gray-300">
+                      <Info size={15} />
+                      <span>Please select preffered payment method</span>
+                    </div>
+                    <div className="mb-5 ml-5">
+                      <RadioSelect
+                        name="payment_type"
+                        options={PAYMENT_OPTIONS}
+                      />
+                    </div>
+                    <div className="mt-4 bg-brand-100 p-4 mb-4 rounded-lg text-[12px]">
+                      <div className="grid grid-cols-2 py-2 text-black gap-y-3 md:grid-cols-3">
+                        <div className="md:col-span-2 justify-self-start">
+                          Cost of service
+                        </div>
+                        <InputFieldFormatted
+                          formatAsNaira
+                          name="costOfService"
+                          className="justify-self-end "
+                        />
+                        <div className="md:col-span-2 justify-self-start">
+                          Amount Paid
+                        </div>
+                        <InputFieldFormatted
+                          formatAsNaira
+                          name="amountPaid"
+                          className="justify-self-end"
+                        />
+                        <div className="justify-self-start md:col-span-2">
+                          Balance
+                        </div>
+                        <div className="font-bold justify-self-end">
+                          {formatPrice(
+                            (values.costOfService || 0) -
+                              (values.amountPaid || 0)
+                          )}
+                        </div>
+                        <div className="flex justify-between col-span-2 p-2 mt-2 font-bold text-black bg-white border border-gray-300 rounded-lg md:col-span-3">
+                          <span>Total Amount</span>
+                          <span>{formatPrice(values.costOfService || 0)}</span>
+                        </div>
                       </div>
                     </div>
-                  </div>
 
-                  {/* Submit Button */}
-                  <Button
-                    label="Create Order"
-                    type="submit"
-                    disabled={!isValid || orderItems.length === 0 || isPending}
-                    isLoading={isPending}
-                  />
-                </Form>
-              )}
+                    {/* Submit Button */}
+                    <Button
+                      label="Create Order"
+                      type="submit"
+                      disabled={
+                        !isValid || orderItems.length === 0 || isPending
+                      }
+                      isLoading={isPending}
+                    />
+                  </Form>
+                );
+              }}
             </Formik>
           </div>
         </div>
--- src/pages/orders/AddOrderNewUser.tsx
+++ src/pages/orders/AddOrderNewUser.tsx
@@ -32,13 +32,18 @@ export const AddOrderNewUser: React.FC = () => {
   const { mutate: createOrder, isPending } = useCreateOrder();
   const { data } = useGetOrderItem();
   const orderItems = data?.Items ?? [];
+  const cost_of_service = orderItems.reduce(
+    (x, item) => x + Number(item.price),
+    0
+  );
   const initialValues = {
     customerName: "",
     customerEmail: "",
     phoneNumber: "",
+    craetionDate: new Date(),
     pickupDate: new Date(),
     payment_type: "cash",
-    costOfService: 0,
+    costOfService: cost_of_service,
     amountPaid: 0,
   };
 
@@ -66,7 +71,7 @@ export const AddOrderNewUser: React.FC = () => {
 
     try {
       const formData = new FormData();
-
+      const totalAmount = Number(values.costOfService) - values.amountPaid;
       formData.append("name", values.customerName);
       formData.append("email", values.customerEmail);
       formData.append("payment_type", values.payment_type);
@@ -74,10 +79,7 @@ export const AddOrderNewUser: React.FC = () => {
       formData.append("pickup_date", values.pickupDate?.toISOString() || "");
       formData.append("order_type", "2");
       formData.append("is_exist", "0");
-      formData.append(
-        "total_amount",
-        String(values.costOfService - values.amountPaid)
-      );
+      formData.append("total_amount", String(totalAmount));
       formData.append("paid_amount", String(values.amountPaid));
       // formData.append("items_ids", String(itemIDs));
       orderItems.forEach((item) => {
@@ -173,129 +175,144 @@ export const AddOrderNewUser: React.FC = () => {
               onSubmit={handleSubmit}
               // enableReinitialize
             >
-              {({ isValid, values }) => (
-                <Form className="w-full space-y-2">
-                  <InputField
-                    type="text"
-                    placeholder="Enter customer name"
-                    name="customerName"
-                  />
-                  <InputField
-                    type="email"
-                    placeholder="Enter customer Email"
-                    name="customerEmail"
-                  />
-                  <InputField
-                    type="number"
-                    placeholder="Customer Phone No."
-                    name="phoneNumber"
-                  />
-
-                  {/* Added Item */}
-                  <div className="flex items-center justify-between w-full py-3 mt-2 text-black">
-                    <div className="font-bold text-[16px]">Add Item</div>
-                    <FiPlusCircle
-                      className="w-8 h-8 text-black cursor-pointer"
-                      onClick={() => {
-                        modal.openModal(<AddItemForOrder />);
-                      }}
+              {({ isValid, values, setFieldValue }) => {
+                // eslint-disable-next-line react-hooks/rules-of-hooks
+                React.useEffect(() => {
+                  setFieldValue("costOfService", cost_of_service);
+                  // eslint-disable-next-line react-hooks/exhaustive-deps
+                }, [cost_of_service, setFieldValue]);
+                return (
+                  <Form className="w-full space-y-2">
+                    <InputField
+                      type="text"
+                      placeholder="Enter customer name"
+                      name="customerName"
                     />
-                  </div>
+                    <InputField
+                      type="email"
+                      placeholder="Enter customer Email"
+                      name="customerEmail"
+                    />
+                    <InputField
+                      type="number"
+                      placeholder="Customer Phone No."
+                      name="phoneNumber"
+                    />
 
-                  {orderItems.map((item, index) => (
-                    <div
-                      className="flex items-center justify-between px-4 py-2 mt-1 rounded-lg cursor-pointer bg-brand-100"
-                      key={index}
-                      onClick={() => handleEditItem(item)}
-                    >
-                      <div className="flex items-center gap-4">
-                        <img
-                          src="/images/order-icon.png"
-                          className="object-cover w-10 h-10 rounded"
-                          alt={item.item_type}
+                    {/* Added Item */}
+                    <div className="flex items-center justify-between w-full py-3 mt-2 text-black">
+                      <div className="font-bold text-[16px]">Add Item</div>
+                      <FiPlusCircle
+                        className="w-8 h-8 text-black cursor-pointer"
+                        onClick={() => {
+                          modal.openModal(<AddItemForOrder />);
+                        }}
+                      />
+                    </div>
+
+                    {orderItems.map((item, index) => (
+                      <div
+                        className="flex items-center justify-between px-4 py-2 mt-1 rounded-lg cursor-pointer bg-brand-100"
+                        key={index}
+                        onClick={() => handleEditItem(item)}
+                      >
+                        <div className="flex items-center gap-4">
+                          <img
+                            src="/images/order-icon.png"
+                            className="object-cover w-10 h-10 rounded"
+                            alt={item.item_type}
+                          />
+                          <div className="text-left">
+                            <p className="font-semibold text-quick-action-icon">
+                              {item.service_name}
+                            </p>
+                            <p className="text-sm text-gray-500">
+                              {item.item_type} - {item.no_of_items} pieces
+                            </p>
+                          </div>
+                        </div>
+                        <div className="">
+                          <div className="flex items-center justify-end gap-1 text-sm cursor-pointer text-quick-action-icon hover:text-blue-700">
+                            <ChevronRight />
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+
+                    {/* Pickup Date */}
+                    <div className="mt-4 text-left">
+                      <DatePickerInput
+                        label="Creation Date"
+                        name="creationDate"
+                        maxDate={new Date()}
+                      />
+                      <DatePickerInput
+                        label="Pickup Date"
+                        name="pickupDate"
+                        minDate={new Date()}
+                      />
+                    </div>
+
+                    {/* Payment Details */}
+                    <div className="text-black font-bold text-[16px] mb-0 text-left mt-5">
+                      Payment Method
+                    </div>
+                    <div className="flex items-center gap-1 text-sm text-gray-300">
+                      <Info size={15} />
+                      <span>Please select preffered payment method</span>
+                    </div>
+                    <div className="mb-5 ml-5">
+                      <RadioSelect
+                        name="payment_type"
+                        options={PAYMENT_OPTIONS}
+                      />
+                    </div>
+                    <div className="mt-4 bg-brand-100 p-4 mb-4 rounded-lg text-[12px]">
+                      <div className="grid grid-cols-2 py-2 text-black gap-y-3 md:grid-cols-3">
+                        <div className="md:col-span-2 justify-self-start">
+                          Cost of service
+                        </div>
+                        <InputFieldFormatted
+                          formatAsNaira
+                          name="costOfService"
+                          className="justify-self-end "
                         />
-                        <div className="text-left">
-                          <p className="font-semibold text-quick-action-icon">
-                            {item.service_name}
-                          </p>
-                          <p className="text-sm text-gray-500">
-                            {item.item_type} - {item.no_of_items} pieces
-                          </p>
+                        <div className="md:col-span-2 justify-self-start">
+                          Amount Paid
                         </div>
-                      </div>
-                      <div className="">
-                        <div className="flex items-center justify-end gap-1 text-sm cursor-pointer text-quick-action-icon hover:text-blue-700">
-                          <ChevronRight />
+                        <InputFieldFormatted
+                          formatAsNaira
+                          name="amountPaid"
+                          className="justify-self-end"
+                        />
+                        <div className="justify-self-start md:col-span-2">
+                          Balance
+                        </div>
+                        <div className="font-bold justify-self-end">
+                          {formatPrice(
+                            (values.costOfService || 0) -
+                              (values.amountPaid || 0)
+                          )}
+                        </div>
+                        <div className="flex justify-between col-span-2 p-2 mt-2 font-bold text-black bg-white border border-gray-300 rounded-lg md:col-span-3">
+                          <span>Total Amount</span>
+                          <span>{formatPrice(values.costOfService || 0)}</span>
                         </div>
                       </div>
                     </div>
-                  ))}
 
-                  {/* Pickup Date */}
-                  <div className="mt-4 text-left">
-                    <DatePickerInput
-                      label="Pickup Date"
-                      name="pickupDate"
-                      minDate={new Date()}
+                    {/* Submit Button */}
+                    <Button
+                      label="Create Order"
+                      type="submit"
+                      disabled={
+                        !isValid || orderItems.length === 0 || isPending
+                      }
+                      isLoading={isPending}
                     />
-                  </div>
-
-                  {/* Payment Details */}
-                  <div className="text-black font-bold text-[16px] mb-0 text-left mt-5">
-                    Payment Method
-                  </div>
-                  <div className="flex items-center gap-1 text-sm text-gray-300">
-                    <Info size={15} />
-                    <span>Please select preffered payment method</span>
-                  </div>
-                  <div className="mb-5 ml-5">
-                    <RadioSelect
-                      name="payment_type"
-                      options={PAYMENT_OPTIONS}
-                    />
-                  </div>
-                  <div className="mt-4 bg-brand-100 p-4 mb-4 rounded-lg text-[12px]">
-                    <div className="grid grid-cols-2 py-2 text-black gap-y-3 md:grid-cols-3">
-                      <div className="md:col-span-2 justify-self-start">
-                        Cost of service
-                      </div>
-                      <InputFieldFormatted
-                        formatAsNaira
-                        name="costOfService"
-                        className="justify-self-end "
-                      />
-                      <div className="md:col-span-2 justify-self-start">
-                        Amount Paid
-                      </div>
-                      <InputFieldFormatted
-                        formatAsNaira
-                        name="amountPaid"
-                        className="justify-self-end"
-                      />
-                      <div className="justify-self-start md:col-span-2">
-                        Balance
-                      </div>
-                      <div className="font-bold justify-self-end">
-                        {formatPrice(
-                          (values.costOfService || 0) - (values.amountPaid || 0)
-                        )}
-                      </div>
-                      <div className="flex justify-between col-span-2 p-2 mt-2 font-bold text-black bg-white border border-gray-300 rounded-lg md:col-span-3">
-                        <span>Total Amount</span>
-                        <span>{formatPrice(values.costOfService || 0)}</span>
-                      </div>
-                    </div>
-                  </div>
-
-                  {/* Submit Button */}
-                  <Button
-                    label="Create Order"
-                    type="submit"
-                    disabled={!isValid || orderItems.length === 0 || isPending}
-                    isLoading={isPending}
-                  />
-                </Form>
-              )}
+                  </Form>
+                );
+              }}
             </Formik>
           </div>
         </div>
